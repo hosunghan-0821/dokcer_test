8551a9cac660e943afda63862dfa6c826118a5a3
hosung <winsomed96@naver.com>
2023-10-13 11:27:29 +0900

    fe: test

--- src/main/java/com/example/test/testController.java
+++ src/main/java/com/example/test/testController.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class testController {
 
+    @Value("${test}")
+    private String myVariable;
+
     @GetMapping("/test1")
     public String test() {
-        return "test_!12555344zxc123";
+        return myVariable;
     }
 }
